045bc8a47682bda977b007df4ac10e9f236bae9e
Gautham-Ramkumar03 <166893970+Gautham-Ramkumar03@users.noreply.github.com>
2024-09-27 15:16:44 +0530

    utils.py
    changed libraries of disutils to shutil

--- URDF_Exporter/utils/utils.py
+++ URDF_Exporter/utils/utils.py
@@ -9,40 +9,40 @@ import adsk, adsk.core, adsk.fusion
 import os.path, re
 from xml.etree import ElementTree
 from xml.dom import minidom
-from distutils.dir_util import copy_tree
+import shutil  # Replaced distutils with shutil
 import fileinput
 import sys
 
-def copy_occs(root):    
-    """    
+def copy_occs(root):
+    """
     duplicate all the components
-    """    
+    """
     def copy_body(allOccs, occs):
-        """    
+        """
         copy the old occs to new component
         """
-        
+
         bodies = occs.bRepBodies
         transform = adsk.core.Matrix3D.create()
-        
-        # Create new components from occs
-        # This support even when a component has some occses. 
 
-        new_occs = allOccs.addNewComponent(transform)  # this create new occs
+        # Create new components from occs
+        # This supports even when a component has some occurrences.
+
+        new_occs = allOccs.addNewComponent(transform)  # this creates new occs
         if occs.component.name == 'base_link':
             occs.component.name = 'old_component'
             new_occs.component.name = 'base_link'
         else:
             new_occs.component.name = re.sub('[ :()]', '_', occs.name)
-        new_occs = allOccs.item((allOccs.count-1))
+        new_occs = allOccs.item((allOccs.count - 1))
         for i in range(bodies.count):
             body = bodies.item(i)
             body.copyToComponent(new_occs)
-    
+
     allOccs = root.occurrences
     oldOccs = []
-    coppy_list = [occs for occs in allOccs]
-    for occs in coppy_list:
+    copy_list = [occs for occs in allOccs]
+    for occs in copy_list:
         if occs.bRepBodies.count > 0:
             copy_body(allOccs, occs)
             oldOccs.append(occs)
@@ -51,10 +51,9 @@ def copy_occs(root):
         occs.component.name = 'old_component'
 
 
-def export_stl(design, save_dir, components):  
+def export_stl(design, save_dir, components):
     """
-    export stl files into "sace_dir/"
-    
+    export stl files into "save_dir/"
     
     Parameters
     ----------
@@ -63,13 +62,15 @@ def export_stl(design, save_dir, components):
         directory path to save
     components: design.allComponents
     """
-          
+
     # create a single exportManager instance
     exportMgr = design.exportManager
     # get the script location
-    try: os.mkdir(save_dir + '/meshes')
-    except: pass
-    scriptDir = save_dir + '/meshes'  
+    try:
+        os.mkdir(save_dir + '/meshes')
+    except:
+        pass
+    scriptDir = save_dir + '/meshes'
     # export the occurrence one by one in the component to a specified file
     for component in components:
         allOccus = component.allOccurrences
@@ -77,26 +78,26 @@ def export_stl(design, save_dir, components):
             if 'old_component' not in occ.component.name:
                 try:
                     print(occ.component.name)
-                    fileName = scriptDir + "/" + occ.component.name              
+                    fileName = scriptDir + "/" + occ.component.name
                     # create stl exportOptions
                     stlExportOptions = exportMgr.createSTLExportOptions(occ, fileName)
                     stlExportOptions.sendToPrintUtility = False
                     stlExportOptions.isBinaryFormat = True
-                    # options are .MeshRefinementLow .MeshRefinementMedium .MeshRefinementHigh
+                    # options are .MeshRefinementLow, .MeshRefinementMedium, .MeshRefinementHigh
                     stlExportOptions.meshRefinement = adsk.fusion.MeshRefinementSettings.MeshRefinementLow
                     exportMgr.execute(stlExportOptions)
                 except:
-                    print('Component ' + occ.component.name + 'has something wrong.')
-                
+                    print('Component ' + occ.component.name + ' has something wrong.')
 
-def file_dialog(ui):     
+
+def file_dialog(ui):
     """
     display the dialog to save the file
     """
     # Set styles of folder dialog.
     folderDlg = ui.createFolderDialog()
-    folderDlg.title = 'Fusion Folder Dialog' 
-    
+    folderDlg.title = 'Fusion Folder Dialog'
+
     # Show folder dialog
     dlgResult = folderDlg.showDialog()
     if dlgResult == adsk.core.DialogResults.DialogOK:
@@ -106,15 +107,13 @@ def file_dialog(ui):
 
 def origin2center_of_mass(inertia, center_of_mass, mass):
     """
-    convert the moment of the inertia about the world coordinate into 
+    convert the moment of inertia about the world coordinate into
     that about center of mass coordinate
-
-
+    
     Parameters
     ----------
     moment of inertia about the world coordinate:  [xx, yy, zz, xy, yz, xz]
     center_of_mass: [x, y, z]
-    
     
     Returns
     ----------
@@ -123,33 +122,41 @@ def origin2center_of_mass(inertia, center_of_mass, mass):
     x = center_of_mass[0]
     y = center_of_mass[1]
     z = center_of_mass[2]
-    translation_matrix = [y**2+z**2, x**2+z**2, x**2+y**2,
-                         -x*y, -y*z, -x*z]
-    return [ round(i - mass*t, 6) for i, t in zip(inertia, translation_matrix)]
+    translation_matrix = [y**2 + z**2, x**2 + z**2, x**2 + y**2,
+                         -x * y, -y * z, -x * z]
+    return [round(i - mass * t, 6) for i, t in zip(inertia, translation_matrix)]
 
 
 def prettify(elem):
     """
     Return a pretty-printed XML string for the Element.
+    
     Parameters
     ----------
     elem : xml.etree.ElementTree.Element
     
-    
     Returns
     ----------
-    pretified xml : str
+    prettified xml : str
     """
     rough_string = ElementTree.tostring(elem, 'utf-8')
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent="  ")
 
+
 def copy_package(save_dir, package_dir):
-    try: os.mkdir(save_dir + '/launch')
-    except: pass 
-    try: os.mkdir(save_dir + '/urdf')
-    except: pass 
-    copy_tree(package_dir, save_dir)
+    try:
+        os.mkdir(save_dir + '/launch')
+    except:
+        pass
+    try:
+        os.mkdir(save_dir + '/urdf')
+    except:
+        pass
+    
+    # Refactor: using shutil.copytree in place of distutils.copy_tree
+    shutil.copytree(package_dir, save_dir, dirs_exist_ok=True)  # Allows overwriting existing directories
+
 
 def update_cmakelists(save_dir, package_name):
     file_name = save_dir + '/CMakeLists.txt'
@@ -159,6 +166,7 @@ def update_cmakelists(save_dir, package_name):
             sys.stdout.write("project(" + package_name + ")\n")
         else:
             sys.stdout.write(line)
+
 
 def update_package_xml(save_dir, package_name):
     file_name = save_dir + '/package.xml'
@@ -170,4 +178,3 @@ def update_package_xml(save_dir, package_name):
             sys.stdout.write("<description>The " + package_name + " package</description>\n")
         else:
             sys.stdout.write(line)
-        
